3533d1c61c88b17e62f1280d958cef941b92a8b9
Amanda Werner <amandaewerner9@gmail.com>
2025-05-23 16:23:20 -0300

    Create matriz.php

---
+++ aula4/matriz.php
@@ -0,0 +1,12 @@
+
+<?php 
+
+$v1 = array(10, 12, 15);
+$v2 = array(22, 21, 4);
+$v3 = array(5, 8, 54);
+
+$matriz = array($v1, $v2, $v3);
+
+//imprimir o elemento da 2 linha e 3 colona
+echo $matriz[1][2];
+echo "\n"; 

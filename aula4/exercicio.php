6742e10999d56e38e78b421e64aaffde51f5fbbb
Amanda Werner <amandaewerner9@gmail.com>
2025-05-23 16:23:43 -0300

    Create exercicio.php

---
+++ aula4/exercicio.php
@@ -0,0 +1,31 @@
+<?php
+
+$cores = array("rosa", "azul", "preto", "verde", "roxo");
+$cabelos = array("liso", "ondulado", "cacheado", "crespo", "longo");
+$esportes = array("futebol", "volei", "basquete", "tenis", "handebol");
+$acessorios = array("anel", "colar", "pulseira", "brinco", "tiara");
+
+foreach($cores as $x){
+    echo "$x";
+    echo " | ";
+}
+echo "\n";
+
+foreach($cabelos as $y){
+    echo "$y";
+    echo " | ";
+}
+echo "\n";
+
+foreach($esportes as $w){
+    echo "$w";
+    echo " | ";
+}
+echo "\n";
+
+
+foreach($acessorios as $p){
+    echo "$p";
+    echo " | ";
+}
+echo "\n";

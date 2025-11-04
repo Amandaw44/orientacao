1e4387c97672044fe00048871472066c5cd0e89e
Amanda Werner <amandaewerner9@gmail.com>
2025-11-04 14:11:37 -0300

    Add getNotaFinal method to Participacao class

--- aula11/exercicio/Participacao.php
+++ aula11/exercicio/Participacao.php
@@ -1,5 +1,16 @@
 <?php
 
 class Participacao extends Instrumento{
+     
+    public function getNotaFinal()
+    {
+        $notaFinal = $this->getNota();
+        if($notaFinal > 10){
+            return 10;
+        }else{
+            return $notaFinal;
+            
+        }
+    }
     
 }
